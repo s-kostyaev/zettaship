f4e4d94788c136e2e40351c030cdd283a4a3e7f6
Kostyaev Sergey <s.kostyaev@office.ngs.ru>
2014-11-25 12:04:34 +0600

    add script for creating deb

---
+++ create-deb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo "Usage:"
+    echo "$0 version"
+    exit 1
+fi
+sed -i 's/new_ver/'$1'/g' zettaship-git/DEBIAN/control
+mkdir -p zettaship-git/etc
+mkdir -p zettaship-git/usr/bin
+git clone https://github.com/s-kostyaev/zettaship.git
+go get github.com/BurntSushi/toml
+go get github.com/crackcomm/go-clitable
+go get github.com/op/go-logging
+cd zettaship
+git checkout dev
+cd zfs
+go build
+cp zfs ../../zettaship-git/usr/bin/
+cp zettaship.toml ../../zettaship-git/etc/
+cd ../..
+sed -i 's/'$1'/new_ver/g' zettaship-git/DEBIAN/control
+dpkg -b zettaship-git zettaship-git_$1_amd64.deb
